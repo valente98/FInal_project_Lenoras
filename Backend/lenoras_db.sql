54b37705cb1d83f5ed3640015265b82b223f42ca
valente alvarez <valvarez@basecampcodingacademy.org>
2018-03-16 14:17:37 -0500

    created a file that will connect to the DataBase. then work on the sql file which created the table in the database.

--- Backend/lenoras_db.sql
+++ Backend/lenoras_db.sql
@@ -1,25 +1,25 @@
 CREATE TABLE manager(
-    manage_id SERIAL Primary Key,
+    manage_id SERIAL primary Key,
     username text,
     Password_hash text,
     First_name text,
     Last_name text,
-)
+    manager_type text
+);
 
 CREATE TABLE Owner(
-    Owner_Id Primary key,
     username text,
     Password_hash text,
     first_name text,
-    Last_name text,
-)
+    Last_name text
+);
 
 CREATE TABLE Reservation(
-    Costumer_id SERIAL Primary Key,
+    Costumer_id SERIAL primary Key,
     first_name text,
-    Last_name
+    Last_name text,
     costumer_number numeric,
     amount_people numeric,
     what_date date,
-    what_time time, 
-)
+    what_time time 
+);
